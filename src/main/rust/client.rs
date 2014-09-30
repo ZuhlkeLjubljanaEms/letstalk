96b1352fd29117ebfa1a7e56ba6eedba0f538cc4
astrals <and@zuhlke.com>
2014-09-30 13:45:33 +0100

    takes message from command line

--- src/main/rust/client.rs
+++ src/main/rust/client.rs
@@ -3,15 +3,22 @@ use std::os;
 
 fn main() {
 
-	let mut addr = "127.0.0.1";
 	 let args = os::args();
-	 if args.len() > 1 {
-	 	addr = args[1].as_slice();
+	 if args.len() < 3 {
+	 	println!("use: client addr message");
 	 }
+	let addr = args[1].as_slice();
 
 	let mut socket = TcpStream::connect(addr, 7777).unwrap();
 	
-	let buf:[u8, ..6] = [65, 66, 67, 68, 69, 70];
-	socket.write(buf);
+//	let s = args[2].clone();
+//	let buf = s.into_bytes();
+//	socket.write(buf.as_slice());
+	
+//	let s = args[2].clone();
+//	let buf = s.into_bytes();
+	socket.write(args[2].clone().into_bytes().as_slice());
+	
+	
 //	let response = socket.read_to_end();
 }
