21fec70280e81307a90fc5ebc84aad2156a01b7c
jeremyhannon <hje@zuhlke.com>
2014-09-30 18:31:24 +0200

    JSON Read and write functions ready for integration.

--- src/common/fileIO.rs
+++ src/common/fileIO.rs
@@ -11,8 +11,6 @@ extern crate serialize;
 use serialize::json;
 use std::io::{File, Open, Write, BufferedReader};
 
-
-
 // Automatically generate `Decodable` and `Encodable` trait implementations
 #[deriving(Decodable, Encodable)]
 pub struct FriendInfo  {
@@ -23,11 +21,11 @@ pub struct FriendInfo  {
 static FRIEND_LIST_FILENAME: &'static str = "friendList.json";
 
 fn main() {
-    write_json_to_file();
-    read_friends_from_file(FRIEND_LIST_FILENAME);
+    write_dummy_json_to_file(FRIEND_LIST_FILENAME);
+    let mut stored_friend_info: Vec<FriendInfo> = read_friends_from_file(FRIEND_LIST_FILENAME);
 }
 
-fn write_json_to_file() {
+fn write_dummy_json_to_file(filename: &str) {
     let will_friend_info = FriendInfo {
         friend_nickname: "Will43".to_string(),
         friend_last_ip_address: "192.168.20.151".to_string(),
@@ -36,7 +34,7 @@ fn write_json_to_file() {
     // Serialize using `json::encode`
     let encoded_friend_info = json::encode(&will_friend_info);
     
-    let path_to_friend_list_file = Path::new(FRIEND_LIST_FILENAME);
+    let path_to_friend_list_file = Path::new(filename);
 
     let mut friend_list_file = match File::open_mode(&path_to_friend_list_file, Open, Write) {
             Ok(friend_list_file) => friend_list_file,
@@ -47,7 +45,7 @@ fn write_json_to_file() {
     let _ = friend_list_file.write_line(encoded_friend_info.as_slice());
 }
 
-fn read_friends_from_file(filename: &str) -> () {
+fn read_friends_from_file(filename: &str) -> (Vec<FriendInfo>) {
 
     let path = Path::new(filename);
     let mut file = BufferedReader::new(File::open(&path));
@@ -63,6 +61,7 @@ fn read_friends_from_file(filename: &str) -> () {
     //for n in range(0u, friend_info_collection.len()) {
     //    println!("Vector contains: {}", friend_info_collection.get(n).friend_nickname);
     //}
+    friend_info_collection
 }
 
 
